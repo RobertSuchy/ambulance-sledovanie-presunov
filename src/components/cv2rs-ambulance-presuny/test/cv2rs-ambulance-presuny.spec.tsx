fedef329f5b36200d406a293b7dfcd476209d38b
Róbert Suchý <robert.suchy5@gmail.com>
2024-04-02 23:14:29 +0200

    unit tests

--- src/components/cv2rs-ambulance-presuny/test/cv2rs-ambulance-presuny.spec.tsx
+++ src/components/cv2rs-ambulance-presuny/test/cv2rs-ambulance-presuny.spec.tsx
@@ -7,12 +7,27 @@ describe('cv2rs-ambulance-presuny', () => {
       components: [Cv2rsAmbulancePresuny],
       html: `<cv2rs-ambulance-presuny></cv2rs-ambulance-presuny>`,
     });
+
     expect(page.root).toEqualHtml(`
       <cv2rs-ambulance-presuny>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </cv2rs-ambulance-presuny>
+      <mock:shadow-root>
+        <div>
+          <slot>
+            XSUCHY - AMBULANCE - SLEDOVANIE PRESUNOV
+          </slot>
+        </div>
+        <div>
+          <slot>
+            Povodny text...
+          </slot>
+        </div>
+        <div>
+          <button>
+            Aktualizovat text
+          </button>
+        </div>
+      </mock:shadow-root>
+    </cv2rs-ambulance-presuny>
     `);
   });
 });
